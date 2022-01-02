c6d6ac4b3373e12d63062d26942ab08ff83cb65d
Michael Peter Rixon <michaelpeterrixon@Michaels-MacBook-Air.local>
2022-01-03 09:35:41 +1100

    Finished course

--- demo/src/main/java/com/example/demo/Student/StudentController.java
+++ demo/src/main/java/com/example/demo/Student/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
@@ -24,10 +25,28 @@ public class StudentController {
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
+
+    @GetMapping("/{id}")
+    public void getStudent(@PathVariable("id") long id) {
+        studentService.getStudent(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable("id") long id) {
+        studentService.deleteStudent(id);
+    }
+
+
     @PostMapping
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
     }
 
+    @PutMapping(path = "/{studentId}")
+    public void updateStudent(@PathVariable("studentId") long id,
+                              @RequestParam(required=false) String name,
+                              @RequestParam(required=false) String email) {
+        studentService.updateStudent(id, name, email);
+    }
 
 }
--- demo/src/main/java/com/example/demo/Student/StudentService.java
+++ demo/src/main/java/com/example/demo/Student/StudentService.java
@@ -1,15 +1,12 @@
 package com.example.demo.Student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.Month;
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -25,6 +22,40 @@ public class StudentService {
     @GetMapping
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable("id") long id) {
+        boolean studentExists = studentRepository.existsById(id);
+        if (!studentExists) {
+            throw new IllegalStateException("no student with id " + id);
+        }
+        studentRepository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable("id") long id) {
+        Optional<Student> student = studentRepository.findById(id);
+        if (student.isEmpty()) {
+            throw new IllegalStateException("no student with that id");
+        }
+        return studentRepository.getById(id);
+    }
+
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("student with id " + studentId + " does not exist"));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            student.setEmail(email);
+        }
+
+
     }
 
     @PostMapping
